8c83c3951a471cf1345240270690272b8acedee2
Meet Shah <aitools.meet@gmail.com>
2026-02-28 00:38:51 +0530

    Public Release: Standalone CMS demo

--- src/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import { getSettings } from "@/lib/cms/storage";
-import { FooterClient } from "./FooterClient";
-
-export async function Footer() {
-    const settings = await getSettings();
-
-    return <FooterClient settings={settings} />;
-}
